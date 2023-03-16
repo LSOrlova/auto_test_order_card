09faa6f33288b76d08e5a6f7a05517f6ec8b0fa3
Neto test <lsorlova82@gmail.com>
2023-03-16 15:16:16 +0200

    test added from work

--- src/test/java/ru/netology/orderCardTest.java
+++ src/test/java/ru/netology/orderCardTest.java
@@ -1,11 +1,50 @@
 package ru.netology;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class orderCardTest {
-    
+    private WebDriver driver;
 
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Luyba\\Downloads\\chromedriver_win32\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() throws InterruptedException {
+        //throw new UnsupportedOperationException();
+        driver.get("http://localhost:9999");
+        List<WebElement> elements =
+                driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("alert-success")).getText();
+        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+        Thread.sleep(9000);
+    }
 
 }
