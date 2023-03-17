2a0a8c9a629d30898a9fb5b7dcd9bfc40ed116c9
Luyba <lsorlova82@gmail.com>
2023-03-17 10:53:16 +0200

    negative tests were added

--- src/test/java/ru/netology/WebInterfaceTest.java
+++ src/test/java/ru/netology/WebInterfaceTest.java
@@ -21,7 +21,6 @@ class orderCardTest {
 
     @BeforeAll
     static void setUpAll() {
-//        System.setProperty("webDriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 
@@ -32,6 +31,7 @@ class orderCardTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -41,18 +41,48 @@ class orderCardTest {
     }
 
     @Test
-    void shouldHappyPath() throws InterruptedException {
+    void testHappyPath() throws InterruptedException {
 //        throw new UnsupportedOperationException();
-        driver.get("http://localhost:9999");
+
         List<WebElement> elements =
                 driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Василий");
-        elements.get(1).sendKeys("+79270000000");
+        elements.get(1).sendKeys("+79260006000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__text")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
         Thread.sleep(9000);
+    }
+
+    @Test
+    void testWithoutName() {
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(1).sendKeys("+79260006000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void testWithoutPhone() {
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Коля Бражник");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void testNegativeAgreementEmpty() {
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Макс Сокол");
+        elements.get(1).sendKeys("+79267000000");
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
     }
 
 }
